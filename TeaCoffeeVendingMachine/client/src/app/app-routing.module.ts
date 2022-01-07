a7cac3f32f5425428b7e8c0ca2d327440560d42e
Andrew Vieira <95151710+AndrewChrisV@users.noreply.github.com>
2022-01-07 01:01:09 -0500

    Update app-routing.module.ts
    Added comments

--- TeaCoffeeVendingMachine/client/src/app/app-routing.module.ts
+++ TeaCoffeeVendingMachine/client/src/app/app-routing.module.ts
@@ -3,22 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './core/components/layout/landing/landing.component';
 
 const routes: Routes = [
+  // Landing page (which redirects to home page)
   { path: '', component: LandingComponent },
+  // Home (Choose Drink) page
   {
     path: 'home',
     loadChildren: () =>
       import('./drink/drink.module').then((m) => m.DrinkModule),
   },
+  // Refilling Form page
   {
     path: 'refill',
     loadChildren: () =>
       import('./refill/refill.module').then((m) => m.RefillModule),
   },
+  // Report Options page
   {
     path: 'report',
     loadChildren: () =>
       import('./report/report.module').then((m) => m.ReportModule),
   },
+  // Container Status page
   {
     path: 'container-status',
     loadChildren: () =>
