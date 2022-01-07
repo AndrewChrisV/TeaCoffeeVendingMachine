7591900724b67ca3c3098b3dc9cf3940e4eb7174
Andrew Vieira <95151710+AndrewChrisV@users.noreply.github.com>
2022-01-06 23:58:24 -0500

    Update refill-form.component.ts
    Added comments
    Made max capacities constant values
    Changes blank inputs to 0
    Prevents submission of values that are not nonnegative numbers

--- TeaCoffeeVendingMachine/client/src/app/refill/components/refill-form/refill-form.component.ts
+++ TeaCoffeeVendingMachine/client/src/app/refill/components/refill-form/refill-form.component.ts
@@ -3,6 +3,13 @@ import { Router } from '@angular/router';
 //import { ContainerService } from '../../../container/services/container.service';
 import { RefillService } from '../../services/refill.service';
 
+// maximum capacities of each container
+const maxTea: number = 2;
+const maxCoffee: number = 2;
+const maxSugar: number = 8;
+const maxWater: number = 15;
+const maxMilk: number = 15;
+
 @Component({
   selector: 'app-refill-form',
   templateUrl: './refill-form.component.html',
@@ -13,6 +20,13 @@ export class RefillFormComponent implements OnInit {
   message: string = '';
   //container: any = {};
   err: any = {};
+
+  // maximum capacities of each container (to display on page)
+  maxTeaCapacity: number = maxTea;
+  maxCoffeeCapacity: number = maxCoffee;
+  maxSugarCapacity: number = maxSugar;
+  maxWaterCapacity: number = maxWater;
+  maxMilkCapacity: number = maxMilk;
 
   constructor(
     private refillService: RefillService,
@@ -35,44 +49,73 @@ export class RefillFormComponent implements OnInit {
   }
 
   orderRefillSubmit() {
-    if (this.refill.tea > 2 - 1.89) {
-      //if (this.refill.tea > 2 - this.container.tea) {
+    // Change any blank inputs to 0
+    if (this.refill.tea == null || this.refill.tea == '') this.refill.tea = 0;
+    if (this.refill.coffee == null || this.refill.coffee == '')
+      this.refill.coffee = 0;
+    if (this.refill.sugar == null || this.refill.sugar == '')
+      this.refill.sugar = 0;
+    if (this.refill.water == null || this.refill.water == '')
+      this.refill.water = 0;
+    if (this.refill.milk == null || this.refill.milk == '')
+      this.refill.milk = 0;
+
+    // Check that input values are nonnegative numbers
+    if (isNaN(this.refill.tea) || this.refill.tea < 0)
+      this.message = 'Please enter legitimate value for tea.';
+    else if (isNaN(this.refill.coffee) || this.refill.coffee < 0)
+      this.message = 'Please enter legitimate value for coffee.';
+    else if (isNaN(this.refill.sugar) || this.refill.sugar < 0)
+      this.message = 'Please enter legitimate value for sugar.';
+    else if (isNaN(this.refill.water) || this.refill.water < 0)
+      this.message = 'Please enter legitimate value for water.';
+    else if (isNaN(this.refill.milk) || this.refill.milk < 0) {
+      this.message = 'Please enter legitimate value for milk.';
+      // Prevent ordering too much tea
+    } else if (this.refill.tea > maxTea - 1.89) {
+      //if (this.refill.tea > maxTea - this.container.tea) {
       this.message =
         'You cannot order more than ' +
-        (2 - 1.89).toFixed(3) +
-        //(2 - this.container.tea) +
+        (maxTea - 1.89).toFixed(3) +
+        //(maxTea - this.container.tea) +
         ' kg of tea.';
-    } else if (this.refill.coffee > 2 - 2) {
-      //} else if (this.refill.coffee > 2 - this.container.coffee) {
+      // Prevent ordering too much coffee
+    } else if (this.refill.coffee > maxCoffee - 2) {
+      //} else if (this.refill.coffee > maxCoffee - this.container.coffee) {
       this.message =
         'You cannot order more than ' +
-        (2 - 2).toFixed(3) +
-        //(2 - this.container.coffee) +
+        (maxCoffee - 2).toFixed(3) +
+        //(maxCoffee - this.container.coffee) +
         ' kg of coffee.';
-    } else if (this.refill.sugar > 8 - 7.91) {
-      //} else if (this.refill.sugar > 8 - this.container.sugar) {
+      // Prevent ordering too much sugar
+    } else if (this.refill.sugar > maxSugar - 7.91) {
+      //} else if (this.refill.sugar > maxSugar - this.container.sugar) {
       this.message =
         'You cannot order more than ' +
-        (8 - 7.91).toFixed(3) +
-        //(8 - this.container.sugar) +
+        (maxSugar - 7.91).toFixed(3) +
+        //(maxSugar - this.container.sugar) +
         ' kg of sugar.';
-    } else if (this.refill.water > 15 - 14.17) {
-      //} else if (this.refill.water > 2 - this.container.water) {
+      // Prevent ordering too much water
+    } else if (this.refill.water > maxWater - 14.17) {
+      //} else if (this.refill.water > maxWater - this.container.water) {
       this.message =
         'You cannot order more than ' +
-        (15 - 14.17).toFixed(3) +
-        //(15 - this.container.water)
+        (maxWater - 14.17).toFixed(3) +
+        //(maxWater - this.container.water)
         ' liters of water.';
-    } else if (this.refill.milk > 15 - 14.39) {
-      //} else if (this.refill.milk > 15 - this.container.milk) {
+      // Prevent ordering too much milk
+    } else if (this.refill.milk > maxMilk - 14.39) {
+      //} else if (this.refill.milk > maxMilk - this.container.milk) {
       this.message =
         'You cannot order more than ' +
-        (15 - 14.39).toFixed(3) +
-        //(15 - this.container.milk)
+        (maxMilk - 14.39).toFixed(3) +
+        //(maxMilk - this.container.milk)
         ' liters of milk.';
     } else {
       this.message = '';
+      // Display refill order in console
       console.log(JSON.stringify(this.refill));
+      // Submit refill to database
       this.refillService.orderRefill(this.refill).subscribe(
         (refill) => {},
         (err) => {
