72070eda34c0cf3a47ff2a594e45ac0d461d0551
Andrew Vieira <95151710+AndrewChrisV@users.noreply.github.com>
2022-01-07 01:08:51 -0500

    Update report.service.ts
    Added comments

--- TeaCoffeeVendingMachine/client/src/app/report/services/report.service.ts
+++ TeaCoffeeVendingMachine/client/src/app/report/services/report.service.ts
@@ -8,14 +8,17 @@ import { Observable } from 'rxjs';
 export class ReportService {
   constructor(private httpClient: HttpClient) {}
 
+  // Get refilling counter details from database
   getCounter(): Observable<any> {
     return this.httpClient.get('api/counter');
   }
 
+  // Get total sales for all days from database
   getSalesAllDays(): Observable<any> {
     return this.httpClient.get('api/sales/all-days');
   }
 
+  // Get total sales for today from database
   getSalesToday(): Observable<any> {
     return this.httpClient.get('api/sales/today');
   }
