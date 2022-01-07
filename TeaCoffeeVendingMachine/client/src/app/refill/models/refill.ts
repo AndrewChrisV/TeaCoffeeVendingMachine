3ce5ba32039f94694319d9910ea5737168455f4d
Andrew Vieira <95151710+AndrewChrisV@users.noreply.github.com>
2022-01-07 00:07:34 -0500

    Update refill.ts
    Edited comments

--- TeaCoffeeVendingMachine/client/src/app/refill/models/refill.ts
+++ TeaCoffeeVendingMachine/client/src/app/refill/models/refill.ts
@@ -1,7 +1,7 @@
 export class Refill {
-  tea: number; // amount of tea ordered
-  coffee: number; // amount of coffee ordered
-  sugar: number; // amount of sugar ordered
-  water: number; // amount of water ordered
-  milk: number; // amount of milk ordered
+  tea: number; // kg of tea ordered
+  coffee: number; // kg of coffee ordered
+  sugar: number; // kg of sugar ordered
+  water: number; // liters of water ordered
+  milk: number; // liters of milk ordered
 }
