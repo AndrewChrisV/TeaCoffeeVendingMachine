773bab1937d942c008c2683ea737f74335d32e77
Andrew Vieira <95151710+AndrewChrisV@users.noreply.github.com>
2022-01-07 01:19:56 -0500

    Update counter.ts
    Added comments

--- TeaCoffeeVendingMachine/client/src/app/report/models/counter.ts
+++ TeaCoffeeVendingMachine/client/src/app/report/models/counter.ts
@@ -1,12 +1,12 @@
 export class Counter {
-  teaAllDays: number;
-  teaToday: number;
-  coffeeAllDays: number;
-  coffeeToday: number;
-  sugarAllDays: number;
-  sugarToday: number;
-  waterAllDays: number;
-  waterToday: number;
-  milkAllDays: number;
-  milkToday: number;
+  teaAllDays: number; // times tea container was refilled ever
+  teaToday: number; // times tea container was refilled today
+  coffeeAllDays: number; // times coffee container was refilled ever
+  coffeeToday: number; // times coffee container was refilled today
+  sugarAllDays: number; // times sugar container was refilled ever
+  sugarToday: number; // times sugar container was refilled today
+  waterAllDays: number; // times water container was refilled ever
+  waterToday: number; // times water container was refilled today
+  milkAllDays: number; // times milk container was refilled ever
+  milkToday: number; // times milk container was refilled today
 }
