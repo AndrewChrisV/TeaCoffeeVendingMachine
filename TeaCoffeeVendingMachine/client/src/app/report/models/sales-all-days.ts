1d28fb21ce9270626ffcdd0a7ccd09068012506c
Andrew Vieira <95151710+AndrewChrisV@users.noreply.github.com>
2022-01-07 01:15:00 -0500

    Update sales-all-days.ts
    Added comments

--- TeaCoffeeVendingMachine/client/src/app/report/models/sales-all-days.ts
+++ TeaCoffeeVendingMachine/client/src/app/report/models/sales-all-days.ts
@@ -1,6 +1,6 @@
 export class SalesAllDays {
-  tea: number;
-  blackTea: number;
-  coffee: number;
-  blackCoffee: number;
+  tea: number; // cups of tea sold over all days
+  blackTea: number; // cups of black tea sold over all days
+  coffee: number; // cups of coffee sold over all days
+  blackCoffee: number; // cups of black coffee sold over all days
 }
