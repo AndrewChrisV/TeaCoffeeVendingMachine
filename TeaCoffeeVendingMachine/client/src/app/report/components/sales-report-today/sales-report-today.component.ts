94b42e63188f854912fb335819ccf4d7d75f8bc4
Andrew Vieira <95151710+AndrewChrisV@users.noreply.github.com>
2022-01-07 01:56:18 -0500

    Update sales-report-today.component.ts
    Added comments
    Made drink costs variables in SalesReportTodayComponent

--- TeaCoffeeVendingMachine/client/src/app/report/components/sales-report-today/sales-report-today.component.ts
+++ TeaCoffeeVendingMachine/client/src/app/report/components/sales-report-today/sales-report-today.component.ts
@@ -11,12 +11,20 @@ export class SalesReportTodayComponent implements OnInit {
   salesToday: any = {};
   err: any = {};
 
+  // prices (in Rs.) per cup of each type of drink
+  costTea: number = 10;
+  costBlackTea: number = 5;
+  costCoffee: number = 15;
+  costBlackCoffee: number = 10;
+
   constructor(private reportService: ReportService, private router: Router) {}
 
   ngOnInit(): void {
     this.reportService.getSalesToday().subscribe(
       (req) => {
+        // Get sales report for today from database
         this.salesToday = req;
+        // Display contents of report to console
         console.log(JSON.stringify(this.salesToday));
       },
       (err) => {
